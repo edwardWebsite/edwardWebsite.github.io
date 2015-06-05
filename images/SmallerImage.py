f888f04a67048e44f523a17cc3e6b9030a8ad498
laurencee9 <edward.laurence.1@ulaval.ca>
2015-06-05 14:31:59 +0200

    New image

--- images/SmallerImage.py
+++ images/SmallerImage.py
@@ -5,14 +5,14 @@ import ExifTags
 
 def GenerationImageThumbnail(folderName):
 
-	MAXWIDTH = 200.0;
+	MAXWIDTH = 350.0;
 	for infile in os.listdir(folderName):
 		outfile = os.path.splitext(infile)[0] + "thumbnail.JPG"
 		if infile != outfile:
 			try:
 				im = Image.open(folderName+infile)
 				s = im.size
-				ratio = MAXWIDTH/s[0];
+				ratio = MAXWIDTH/s[1];
 				# print(ratio)
 				im.thumbnail((int(round(s[0]*ratio)), int(round(s[1]*ratio))), Image.ANTIALIAS)
 				im.save(folderName+outfile, "JPEG")
